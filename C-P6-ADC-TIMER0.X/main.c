686d99cdd4798185fdd01fc5e132a2805db8f8f3
jeloyah <jeloyah@gmail.com>
2025-10-14 09:55:08 -0700

    Update main.c

--- C-P6-ADC-TIMER0.X/main.c
+++ C-P6-ADC-TIMER0.X/main.c
@@ -4,13 +4,9 @@
 #include "clock.h"
 #include "gpio.h"
 #include "adc.h"
-#include "timer0.h"
 
 void main(void) {
     
-    unsigned char i;
-    unsigned int ADCsample[16];
-    unsigned char ADCresult;
     
     /* System Clock Initialization */
 	CLOCK_Initialize();
@@ -35,31 +31,17 @@ void main(void) {
     
     /* Config ADC */
     ADC_Initialize();
-    
-    /* Config TIMER0 */
-    TIMER0_Initialize();
         
     /* infinite loop */
     while(1)
     {
              
-     for (i=0; i<16; i++)
-     {
-         /* Start ADC conversion */
-        ADC_Start();
-        
-        TIMER0_Delay_ms(100);
-        
-        /* Wait for Go = 0 */
-        while (ADC_conversionComplete)
-             ;
-        
-        ADCsample[i] += ADRESH;
+
          
      }
      
      
-     ADCresult = (unsigned char) ADCsample / (unsigned int) 16;
+
      
          
      } /* while */
